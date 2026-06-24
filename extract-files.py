ed9d49635d8f925b5c6ec9239ca8452b555fe234
binbindhh <bin@dhao.cc>
2026-06-24 12:01:14 +0800

    sm8750-common: Patch libwfdservice to load android.media.audio.common.types-V5-cpp.so
    Change-Id: I9dc35b265a2cfda8be1c6d229af743f18cb5ce93

--- extract-files.py
+++ extract-files.py
@@ -76,6 +76,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/bin/system_dlkm_modprobe.sh': blob_fixup()
         .regex_replace(r'.*\bzram or zsmalloc\b.*\n', '')
         .regex_replace(r'-e "zram" -e "zsmalloc"', ''),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'vendor/bin/vendor_modprobe.sh': blob_fixup()
         .regex_replace(r'\n.*OPLUS_FEATURE_WIFI_FTM[\s\S]*?OPLUS_FEATURE_WIFI_FTM.*\n', ''),
     'vendor/etc/clstc_config_library.xml': blob_fixup()
