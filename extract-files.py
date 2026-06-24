5e24a111016fda6261ec167de61acce0808845de
binbindhh <bin@dhao.cc>
2026-06-24 22:16:22 +0800

    sm8750-common: Patch wfdservice with libaudiobase

--- extract-files.py
+++ extract-files.py
@@ -80,6 +80,7 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libaudiobase.so')
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .add_needed('libaudiobase.so')
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'vendor/bin/vendor_modprobe.sh': blob_fixup()
         .regex_replace(r'\n.*OPLUS_FEATURE_WIFI_FTM[\s\S]*?OPLUS_FEATURE_WIFI_FTM.*\n', ''),
