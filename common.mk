aca703515b03e8a7b4b18af904937604ef9f541c
Oscar <osm1019@gmail.com>
2026-08-07 08:03:48 -0700

    sm8750-common: Switch to lineage-libperfmgr Power HAL
    Replace QTI power-service + proprietary perfd with
    android.hardware.power-service.lineage-libperfmgr, CAF stub
    libqti-perfd-client, and a sun-specific powerhint.json (2-cluster
    policy0/6, ADPF_DEFAULT day one).
    
    - Add Soong namespaces for google/pixel, lineage power-libperfmgr, stub client
    - Set vendor.powerhal.init=1 on boot_completed
    - Drop dead perfd/iop init dirs and post_boot perfd min dumps
    - Prune # Performance stack from proprietary-files; keep QSPM NDK
      libs for libadreno_app_profiles
    - Keep media_profiles_sun.xml listed (zygote CamcorderProfile needs it)
    
    Change-Id: Ib2d62e6dbb8a8bd2f18059572cefae1d04ca78a8

--- common.mk
+++ common.mk
@@ -349,11 +349,11 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    liboplus-uah-client
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/sun/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QSPA
 PRODUCT_PACKAGES += \
@@ -405,9 +405,11 @@ PRODUCT_SHIPPING_API_LEVEL := 35
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/oplus \
-    hardware/nxp/keymint/generic \
-    hardware/nxp/weaver/generic
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
